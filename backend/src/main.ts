7ca807ccf05f63c0c9ba2c2970cb4032b8137e35
Overhaul <souleymane.fomba7578@gmail.com>
2026-08-21 13:48:28 +0000

    update backend env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://frontend-forest-platform.vercel.app/',
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -9,7 +9,7 @@ export class PrismaService
 {
   constructor() {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string });
+      connectionString: process.env.PRISMA_DATABASE_URL as string });
     super({
       adapter,
       log: ['query', 'warn', 'error'],
